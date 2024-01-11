22d8468ea6249a9cedd798597ca837d87c153aca
Numbers0689 <firewatchsai@gmail.com>
2024-01-12 00:32:03 +0530

    up

--- increasing.py
+++ increasing.py
@@ -1,16 +1,16 @@
 n=input()
 arr=[int(x) for x in input().split(" ")]
 
-def maxx(arr, n):
-    if n == 1:
-        return 0
-    count = prev= 0
-    for i in arr:
-        if i <= prev :
-            prev += 1
-            count += prev-i
-        else:
-            prev = i
-    return count
+# def maxx(arr, n):
+#     if n == 1:
+#         return 0
+#     count = prev= 0
+#     for i in arr:
+#         if i <= prev :
+#             prev += 1
+#             count += prev-i
+#         else:
+#             prev = i
+#     return count
 
-print(maxx(arr, n))      
+# print(maxx(arr, n))
